c4b3ed17378798fb1d741541a4f127cb44bcc7aa
Monica Tamaru Jones <mntamaru@gmail.com>
2020-05-22 13:41:58 -0700

    Collections files add

---
+++ Collections/src/com/example/monica/Main.java
@@ -0,0 +1,26 @@
+package com.example.monica;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Theater theater = new Theater("Strand", 8, 12);
+        //theater.getSeats();
+
+        if (theater.reserveSeat("H11")) {
+            System.out.println("Please pay");
+        }
+        else {
+            System.out.println("Sorry, seat taken");
+        }
+
+        if (theater.reserveSeat("H11")) {
+            System.out.println("Please pay");
+        }
+        else {
+            System.out.println("Sorry, seat taken");
+        }
+
+
+    }
+}
