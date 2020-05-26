ccf2636f2b9a995c8df7f8006c850797ce3d9980
Monica Tamaru Jones <mntamaru@gmail.com>
2020-05-26 14:52:51 -0700

    look at more Collections methods for lists

--- Collections/src/com/example/monica/Main.java
+++ Collections/src/com/example/monica/Main.java
@@ -1,5 +1,9 @@
 package com.example.monica;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,12 +11,12 @@ public class Main {
         Theater theater = new Theater("Strand", 8, 12);
         //theater.getSeats();
 
-        if (theater.reserveSeat("D44")) {
-            System.out.println("Please pay");
-        }
-        else {
-            System.out.println("Sorry, reservation not completed");
-        }
+//        if (theater.reserveSeat("D44")) {
+//            System.out.println("Please pay");
+//        }
+//        else {
+//            System.out.println("Sorry, reservation not completed");
+//        }
 
 //        if (theater.reserveSeat("H11")) {
 //            System.out.println("Please pay");
@@ -22,5 +26,61 @@ public class Main {
 //        }
 
 
+        //shallow copy - contents are same objects/same references
+        List<Theater.Seat> seatCopy = new ArrayList<>(theater.seats);
+        printList(seatCopy);
+
+        seatCopy.get(1).reserve();
+
+        if(theater.reserveSeat("A02")) {
+            System.out.println("Please pay for A02");
+        }
+        else{
+            System.out.println("Seat already reserved");
+        }
+
+//        Collections.reverse(seatCopy);
+//        System.out.println("SeatCopy: ");
+//        printList(seatCopy);
+//        System.out.println("theater.seats: ");
+//        printList(theater.seats);
+
+        Theater.Seat min = Collections.min(seatCopy);
+        Theater.Seat max = Collections.max(seatCopy);
+        System.out.println("Min seat is: " + min.getSeatNumber());
+        System.out.println("Max seat is: " + max.getSeatNumber());
+
+        Collections.shuffle(seatCopy);
+        System.out.println("Shuffled: ");
+        printList(seatCopy);
+
+        sortList(seatCopy);
+        System.out.println("Sorted: ");
+        printList(seatCopy);
+
+
+
+
+    }
+
+    public static void printList(List<Theater.Seat> list) {
+
+        for(Theater.Seat seat : list) {
+            System.out.println(" " + seat.getSeatNumber());
+        }
+
+        System.out.println();
+        System.out.println("============");
+
+    }
+
+    public static void sortList(List<? extends Theater.Seat> list) {
+        for(int i=0; i<list.size()-1; i++) {
+            for(int j=i+1; j<list.size(); j++) {
+                if(list.get(i).compareTo(list.get(j)) > 0) {
+                    Collections.swap(list, i, j);
+                }
+            }
+        }
     }
 }
--- Collections/src/com/example/monica/Theater.java
+++ Collections/src/com/example/monica/Theater.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Theater {
     private final String name;
-    private List<Seat> seats = new ArrayList<>();
+    public List<Seat> seats = new ArrayList<>();
 
     public Theater(String name, int numRows, int seatsPerRow) {
         this.name = name;
@@ -29,6 +29,33 @@ public class Theater {
         Seat requestSeat = new Seat(seatNum);
         // fastest way to find something in a list
         int foundSeat = Collections.binarySearch(seats, requestSeat, null);
+
+        // from Java source code for Collections
+//
+//        int low = 0;
+//        int high = seats.size() - 1;
+//
+//        while (low <= high) {
+//            System.out.println(".");
+//            int mid = (low + high) / 2;
+//            Seat midVal = seats.get(mid);
+//            int comp = midVal.getSeatNumber().compareTo(seatNum);
+//
+//            if (comp < 0) {
+//                low = mid + 1;
+//            }
+//            else if (comp > 0) {
+//                high = mid -1;
+//            }
+//            else {
+//                return seats.get(mid).reserve();
+//            }
+//
+//        // else there is no seat in the theater
+//
+//            System.out.println("There is no seat " + seatNum);
+//            return false;
+//        }
 
         if (foundSeat >= 0) {
             return seats.get(foundSeat).reserve();
@@ -59,7 +86,7 @@ public class Theater {
         }
     }
 
-    private class Seat implements Comparable<Seat>{
+    public class Seat implements Comparable<Seat>{
         private final String seatNumber;
         private boolean reserved = false;
 
