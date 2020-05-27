a11159d215775b702c7d24468c6f3090394d6c67
Monica Tamaru Jones <mntamaru@gmail.com>
2020-05-27 11:30:06 -0700

    add immutability to class

--- Adventure/src/com/example/monica/Location.java
+++ Adventure/src/com/example/monica/Location.java
@@ -4,20 +4,21 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Location {
+    // lets everyone know, don't change these - no setters, as well
     private final int locationID;
     private final String description;
     private final Map<String, Integer> exits;
 
-    public Location(int locationID, String description) {
+    public Location(int locationID, String description, Map<String, Integer> exits) {
         this.locationID = locationID;
         this.description = description;
-        this.exits = new HashMap<String, Integer>();
+        this.exits = new HashMap<String, Integer>(exits);
         this.exits.put("Q", 0);
     }
 
-    public void addExit(String direction, int location) {
-        exits.put(direction, location);
-    }
+//    public void addExit(String direction, int location) {
+//        exits.put(direction, location);
+//    }
 
     public int getLocationID() {
         return locationID;
